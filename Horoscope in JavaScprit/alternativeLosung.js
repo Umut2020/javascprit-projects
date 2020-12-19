c252e72a84c1603879697ea099f724522c5fe78e
Umut <av.umutgungor92@gmail.com>
2020-12-19 21:03:54 +0100

    reorganized

---
+++ Horoscope in JavaScprit/alternativeLosung.js
@@ -0,0 +1,49 @@
+if ((month == 1) && (1 <= day && day <= 21)) {
+  console.log('Burcunuz Oglak')
+} else if ((month == 1) && (22 <= day && day <= 31)) {
+  console.log('Burcunuz Kova')
+}else if ((month == 2) && (1 <= day && day <= 19)) {
+  console.log('Burcunuz Kova')
+}else if ((month == 2) && (20 <= day && day <= 29)) {
+  console.log('Burcunuz Balik')
+}else if ((month == 3) && (1 <= day && day <= 20)) {
+  console.log('Burcunuz Balik')
+}else if ((month == 3) && (21 <= day && day <= 31)) {
+  console.log('Burcunuz Koc')
+}else if ((month == 4) && (1 <= day && day <= 20)) {
+  console.log('Burcunuz Koc')
+}else if ((month == 4) && (21 <= day && day <= 30)) {
+  console.log('Burcunuz Boga')
+}else if ( (month == 5 && 1 <= day && day <= 21)) {
+  console.log('Burcunuz Boga')
+}else if ((month == 5) && (22 <= day && day <= 31)) {
+  console.log('Burcunuz Ikizler')
+}else if ((month == 6) && (1 <= day && day <= 22)) {
+  console.log('Burcunuz Ikizler')
+}else if ((month == 6) && (23 <= day && day <= 30)) {
+  console.log('Burcunuz Yengec')
+}else if ((month == 7) && (1 <= day && day <= 22)) {
+  console.log('Burcunuz Yengec')
+}else if ((month == 7) && (23 <= day && day <= 31)) {
+  console.log('Burcunuz Aslan')
+}else if ((month == 8) && (1 <= day && day <= 22)) {
+  console.log('Burcunuz Aslan')
+}else if ((month == 8) && (23 <= day && day <= 31)) {
+  console.log('Burcunuz Basak')
+}else if ((month == 9) && (1 <= day && day <= 22)) {
+  console.log('Burcunuz Basak')
+}else if ((month == 9) && (23 <= day && day <= 30)) {
+  console.log('Burcunuz Terazi')
+}else if ((month == 10) && (1 <= day && day <= 22)) {
+  console.log('Burcunuz Terazi')
+}else if ((month == 10) && (23 <= day && day <= 31)) {
+  console.log('Burcunuz Akrep')
+}else if ((month == 11) && (1 <= day && day <= 23)) {
+  console.log('Burcunuz Akrep')
+}else if ((month == 11) && (24 <= day && day <= 30)) {
+  console.log('Burcunuz Yay')
+}else if ((month == 12) && (1 <= day && day <= 21)) {
+  console.log('Burcunuz Yay')
+}else if ((month == 12) && (22 <= day && day <= 31)) {
+  console.log('Burcunuz Oglak')
+}
